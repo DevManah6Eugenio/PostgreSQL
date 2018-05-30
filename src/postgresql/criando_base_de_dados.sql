55026b46000a89111eb370fb07b0715bca1c890f
manasses <manasses.java@gmail.com>
2018-05-29 22:34:58 -0300

    criado o bando de dados(primeira atividade do curso de PostgreSQL I)

---
+++ src/postgresql/criando_base_de_dados.sql
@@ -0,0 +1,16 @@
+﻿--criação da base de dados
+CREATE DATABASE controle_compras;
+
+--primeira tabela da base de dados
+CREATE TABLE compras (
+id serial primary key,
+valor numeric,
+data date, 
+observacao varchar(255), 
+recebido smallint);
+
+--inserindo elemento na tebela compras
+INSERT INTO compras(valor, data, observacao, recebido) 
+	VALUES (100, '2018-05-29', 'presente de aniversario', 1);
+
+select * from compras;  
