33019a035075d261a606c980c39bca34ead709bf
Manasses Java <manasses.java@gmail.com>
2018-12-10 10:43:11 -0300

    Update conexoes.sql

--- src/random/conexoes.sql
+++ src/random/conexoes.sql
@@ -1,1 +1,5 @@
+--numero de conexões existentes simultaneamente
+select count(*) from pg_stat_activity;
 
+--listar todas as conexões ativas e seus respectivos processos relacionados
+select * from pg_stat_activity;
